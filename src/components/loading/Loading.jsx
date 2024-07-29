94a98954d605ad507eeb52310063fa628b008865
Samira <sfcds.snf22@uea.edu.br>
2024-07-29 00:27:11 -0400

    loading image

--- src/components/loading/Loading.jsx
+++ src/components/loading/Loading.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Logo from '../../assets/NuBolsoLogo.png'
+import Logo from '../../assets/NBL.png'
 import './Loading.css'
 
 const Loading = () => {
   return (
-    <div className='loafing-container'>
+    <div className='loading-container'>
       <img src={Logo} className='loading-logo' />
     </div>
   )
